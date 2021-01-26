8b928988d0d72a3eee498b3eb8891b79e7082e4f
nakajimamasao <chelseav3@hotmail.com>
2021-01-27 01:28:33 +0900

    2nd Commit

--- android/app/src/main/kotlin/com/nakajimamasao/studyphonics/MainActivity.kt
+++ android/app/src/main/kotlin/com/nakajimamasao/studyphonics/MainActivity.kt
@@ -1,9 +1,10 @@
-package com.nakajimamasao.studyphonics
+package com.nakajimamasao.studyjpword
 
 import io.flutter.embedding.android.FlutterActivity
 
 import android.os.Build
 import android.view.ViewTreeObserver
 import android.view.WindowManager
+
 class MainActivity: FlutterActivity() {
 }
