d2631aef790d0e5d10ad31e236a47838409d23ab
nakajimamasao <chelseav3@hotmail.com>
2021-01-31 10:44:47 +0900

    v1.2.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/services.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -91,7 +93,7 @@ class _MainPageState extends State<MainPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            adMobWidget(),
+            if (!Platform.isIOS) adMobWidget(),
             Spacer(flex: 3),
             Row(
               children: <Widget>[
