27dde5b5449ad806f552260b98e38da5e5f1910d
nakajimamasao <chelseav3@hotmail.com>
2021-07-31 23:24:30 +0900

    v1.3.2

--- lib/main_widget.dart
+++ lib/main_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'ja_word.dart';
 
 class HexColor extends Color {
